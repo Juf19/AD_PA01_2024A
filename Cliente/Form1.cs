d06b4e840d624c4e44dd815929f6b55452664268
Juf19 <jorgeufer.19@gmail.com>
2024-12-03 22:33:08 -0500

    Cambio Cliente

--- Cliente/Form1.cs
+++ Cliente/Form1.cs
@@ -7,6 +7,250 @@ using Protocolo;
 
 namespace Cliente
 {
+    // Código Antiguo
+    /* public partial class FrmValidador : Form
+     {
+         private TcpClient remoto;
+         private NetworkStream flujo;
+
+         public FrmValidador()
+         {
+             InitializeComponent();
+         }
+
+         private void FrmValidador_Load(object sender, EventArgs e)
+         {
+             try
+             {
+                 remoto = new TcpClient("127.0.0.1", 8080);
+                 flujo = remoto.GetStream();
+             }
+             catch (SocketException ex)
+             {
+                 MessageBox.Show("No se puedo establecer conexión " + ex.Message,
+                     "ERROR");
+             }
+             finally 
+             {
+                 flujo?.Close();
+                 remoto?.Close();
+             }
+
+             panPlaca.Enabled = false;
+             chkLunes.Enabled = false;
+             chkMartes.Enabled = false;
+             chkMiercoles.Enabled = false;
+             chkJueves.Enabled = false;
+             chkViernes.Enabled = false;
+             chkDomingo.Enabled = false;
+             chkSabado.Enabled = false;
+         }
+
+         private void btnIniciar_Click(object sender, EventArgs e)
+         {
+             string usuario = txtUsuario.Text;
+             string contraseña = txtPassword.Text;
+             if (usuario == "" || contraseña == "")
+             {
+                 MessageBox.Show("Se requiere el ingreso de usuario y contraseña",
+                     "ADVERTENCIA");
+                 return;
+             }
+
+             Pedido pedido = new Pedido
+             {
+                 Comando = "INGRESO",
+                 Parametros = new[] { usuario, contraseña }
+             };
+
+             Respuesta respuesta = HazOperacion(pedido);
+             if (respuesta == null)
+             {
+                 MessageBox.Show("Hubo un error", "ERROR");
+                 return;
+             }
+
+             if (respuesta.Estado == "OK" && respuesta.Mensaje == "ACCESO_CONCEDIDO")
+             {
+                 panPlaca.Enabled = true;
+                 panLogin.Enabled = false;
+                 MessageBox.Show("Acceso concedido", "INFORMACIÓN");
+                 txtModelo.Focus();
+             }
+             else if (respuesta.Estado == "NOK" && respuesta.Mensaje == "ACCESO_NEGADO")
+             {
+                 panPlaca.Enabled = false;
+                 panLogin.Enabled = true;
+                 MessageBox.Show("No se pudo ingresar, revise credenciales",
+                     "ERROR");
+                 txtUsuario.Focus();
+             }
+         }
+
+         private Respuesta HazOperacion(Pedido pedido)
+         {
+             if(flujo == null)
+             {
+                 MessageBox.Show("No hay conexión", "ERROR");
+                 return null;
+             }
+             try
+             {
+                 byte[] bufferTx = Encoding.UTF8.GetBytes(
+                     pedido.Comando + " " + string.Join(" ", pedido.Parametros));
+
+                 flujo.Write(bufferTx, 0, bufferTx.Length);
+
+                 byte[] bufferRx = new byte[1024];
+
+                 int bytesRx = flujo.Read(bufferRx, 0, bufferRx.Length);
+
+                 string mensaje = Encoding.UTF8.GetString(bufferRx, 0, bytesRx);
+
+                 var partes = mensaje.Split(' ');
+
+                 return new Respuesta
+                 {
+                     Estado = partes[0],
+                     Mensaje = string.Join(" ", partes.Skip(1).ToArray())
+                 };
+             }
+             catch (SocketException ex)
+             {
+                 MessageBox.Show("Error al intentar transmitir " + ex.Message,
+                     "ERROR");
+             }
+             finally 
+             {
+                 flujo?.Close();
+                 remoto?.Close();
+             }
+             return null;
+         }
+
+         private void btnConsultar_Click(object sender, EventArgs e)
+         {
+             string modelo = txtModelo.Text;
+             string marca = txtMarca.Text;
+             string placa = txtPlaca.Text;
+
+             Pedido pedido = new Pedido
+             {
+                 Comando = "CALCULO",
+                 Parametros = new[] { modelo, marca, placa }
+             };
+
+             Respuesta respuesta = HazOperacion(pedido);
+             if (respuesta == null)
+             {
+                 MessageBox.Show("Hubo un error", "ERROR");
+                 return;
+             }
+
+             if (respuesta.Estado == "NOK")
+             {
+                 MessageBox.Show("Error en la solicitud.", "ERROR");
+                 chkLunes.Checked = false;
+                 chkMartes.Checked = false;
+                 chkMiercoles.Checked = false;
+                 chkJueves.Checked = false;
+                 chkViernes.Checked = false;
+             }
+             else
+             {
+                 var partes = respuesta.Mensaje.Split(' ');
+                 MessageBox.Show("Se recibió: " + respuesta.Mensaje,
+                     "INFORMACIÓN");
+                 byte resultado = Byte.Parse(partes[1]);
+                 switch (resultado)
+                 {
+                     case 0b00100000:
+                         chkLunes.Checked = true;
+                         chkMartes.Checked = false;
+                         chkMiercoles.Checked = false;
+                         chkJueves.Checked = false;
+                         chkViernes.Checked = false;
+                         break;
+                     case 0b00010000:
+                         chkMartes.Checked = true;
+                         chkLunes.Checked = false;
+                         chkMiercoles.Checked = false;
+                         chkJueves.Checked = false;
+                         chkViernes.Checked = false;
+                         break;
+                     case 0b00001000:
+                         chkMiercoles.Checked = true;
+                         chkLunes.Checked = false;
+                         chkMartes.Checked = false;
+                         chkJueves.Checked = false;
+                         chkViernes.Checked = false;
+                         break;
+                     case 0b00000100:
+                         chkJueves.Checked = true;
+                         chkLunes.Checked = false;
+                         chkMartes.Checked = false;
+                         chkMiercoles.Checked = false;
+                         chkViernes.Checked = false;
+                         break;
+                     case 0b00000010:
+                         chkViernes.Checked = true;
+                         chkLunes.Checked = false;
+                         chkMartes.Checked = false;
+                         chkMiercoles.Checked = false;
+                         chkJueves.Checked = false;
+                         break;
+                     default:
+                         chkLunes.Checked = false;
+                         chkMartes.Checked = false;
+                         chkMiercoles.Checked = false;
+                         chkJueves.Checked = false;
+                         chkViernes.Checked = false;
+                         break;
+                 }
+             }
+         }
+
+         private void btnNumConsultas_Click(object sender, EventArgs e)
+         {
+             String mensaje = "hola";
+
+             Pedido pedido = new Pedido
+             {
+                 Comando = "CONTADOR",
+                 Parametros = new[] { mensaje }
+             };
+
+             Respuesta respuesta = HazOperacion(pedido);
+             if (respuesta == null)
+             {
+                 MessageBox.Show("Hubo un error", "ERROR");
+                 return;
+             }
+
+             if (respuesta.Estado == "NOK")
+             {
+                 MessageBox.Show("Error en la solicitud.", "ERROR");
+
+             }
+             else
+             {
+                 var partes = respuesta.Mensaje.Split(' ');
+                 MessageBox.Show("El número de pedidos recibidos en este cliente es " + partes[0],
+                     "INFORMACIÓN");
+             }
+         }
+
+         private void FrmValidador_FormClosing(object sender, FormClosingEventArgs e)
+         {
+             if (flujo != null)
+                 flujo.Close();
+             if (remoto != null)
+                 if (remoto.Connected)
+                     remoto.Close();
+         }
+     }*/
+
+    //Código Nuevo
     public partial class FrmValidador : Form
     {
         private TcpClient remoto;
@@ -26,23 +270,9 @@ namespace Cliente
             }
             catch (SocketException ex)
             {
-                MessageBox.Show("No se puedo establecer conexión " + ex.Message,
+                MessageBox.Show("No se pudo establecer conexión " + ex.Message,
                     "ERROR");
             }
-            finally 
-            {
-                flujo?.Close();
-                remoto?.Close();
-            }
-
-            panPlaca.Enabled = false;
-            chkLunes.Enabled = false;
-            chkMartes.Enabled = false;
-            chkMiercoles.Enabled = false;
-            chkJueves.Enabled = false;
-            chkViernes.Enabled = false;
-            chkDomingo.Enabled = false;
-            chkSabado.Enabled = false;
         }
 
         private void btnIniciar_Click(object sender, EventArgs e)
@@ -56,19 +286,22 @@ namespace Cliente
                 return;
             }
 
-            Pedido pedido = new Pedido
+            // Crear el pedido con el nombre de usuario y la contraseña proporcionados por el usuario
+            Protocolos protocolo = new Protocolos(new Pedido
             {
                 Comando = "INGRESO",
                 Parametros = new[] { usuario, contraseña }
-            };
-            
-            Respuesta respuesta = HazOperacion(pedido);
+            });
+
+            // Llamar a HazOperacion para enviar la solicitud al servidor
+            Respuesta respuesta = HazOperacion(protocolo);
             if (respuesta == null)
             {
                 MessageBox.Show("Hubo un error", "ERROR");
                 return;
             }
 
+            // Verificar la respuesta del servidor
             if (respuesta.Estado == "OK" && respuesta.Mensaje == "ACCESO_CONCEDIDO")
             {
                 panPlaca.Enabled = true;
@@ -86,9 +319,9 @@ namespace Cliente
             }
         }
 
-        private Respuesta HazOperacion(Pedido pedido)
+        private Respuesta HazOperacion(Protocolos protocolo)
         {
-            if(flujo == null)
+            if (flujo == null)
             {
                 MessageBox.Show("No hay conexión", "ERROR");
                 return null;
@@ -96,18 +329,19 @@ namespace Cliente
             try
             {
                 byte[] bufferTx = Encoding.UTF8.GetBytes(
-                    pedido.Comando + " " + string.Join(" ", pedido.Parametros));
-                
+                    protocolo.Pedido.Comando + " " + string.Join(" ", protocolo.Pedido.Parametros));
+
                 flujo.Write(bufferTx, 0, bufferTx.Length);
 
                 byte[] bufferRx = new byte[1024];
-                
+
                 int bytesRx = flujo.Read(bufferRx, 0, bufferRx.Length);
-                
+
                 string mensaje = Encoding.UTF8.GetString(bufferRx, 0, bytesRx);
-                
+
                 var partes = mensaje.Split(' ');
-                
+
+                // Pasando la respuesta procesada
                 return new Respuesta
                 {
                     Estado = partes[0],
@@ -119,11 +353,6 @@ namespace Cliente
                 MessageBox.Show("Error al intentar transmitir " + ex.Message,
                     "ERROR");
             }
-            finally 
-            {
-                flujo?.Close();
-                remoto?.Close();
-            }
             return null;
         }
 
@@ -132,14 +361,14 @@ namespace Cliente
             string modelo = txtModelo.Text;
             string marca = txtMarca.Text;
             string placa = txtPlaca.Text;
-            
-            Pedido pedido = new Pedido
+
+            Protocolos protocolo = new Protocolos(new Pedido
             {
                 Comando = "CALCULO",
                 Parametros = new[] { modelo, marca, placa }
-            };
-            
-            Respuesta respuesta = HazOperacion(pedido);
+            });
+
+            Respuesta respuesta = HazOperacion(protocolo);
             if (respuesta == null)
             {
                 MessageBox.Show("Hubo un error", "ERROR");
@@ -212,14 +441,14 @@ namespace Cliente
         private void btnNumConsultas_Click(object sender, EventArgs e)
         {
             String mensaje = "hola";
-            
-            Pedido pedido = new Pedido
+
+            Protocolos protocolo = new Protocolos(new Pedido
             {
                 Comando = "CONTADOR",
                 Parametros = new[] { mensaje }
-            };
+            });
 
-            Respuesta respuesta = HazOperacion(pedido);
+            Respuesta respuesta = HazOperacion(protocolo);
             if (respuesta == null)
             {
                 MessageBox.Show("Hubo un error", "ERROR");
@@ -238,6 +467,7 @@ namespace Cliente
                     "INFORMACIÓN");
             }
         }
+
 
         private void FrmValidador_FormClosing(object sender, FormClosingEventArgs e)
         {
